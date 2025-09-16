6b0714f55b7637caae5d90ba555e8269416322f7
Florian Pasco <106925731+mpek29@users.noreply.github.com>
2025-09-16 17:32:46 +0200

    Update cli.py

--- src/mail_spreader/cli.py
+++ src/mail_spreader/cli.py
@@ -4,6 +4,16 @@ from pathlib import Path
 app = typer.Typer(help="Mail Spreader - utilities to automate LinkedIn-based mailings",pretty_exceptions_enable=False)
 
 @app.command()
+def list_industries_to_linkedin_url(
+    yaml_file_industries: Path = typer.Argument(..., help="YAML industries file"),
+    yaml_file_url: Path = typer.Option(..., "--output", "-o", help="YAML url file")
+):
+    """Convert a list of industries into a LinkedIn URL"""
+    from .scraper import linkedin_url_creator as creator
+
+    creator(yaml_file_industries, yaml_file_url)
+
+@app.command()
 def linkedin_url_to_profil_json(
     yaml_file: Path = typer.Argument(..., help="YAML config file"),
     json_file: Path = typer.Option(..., "--output", "-o", help="Output JSON file")
